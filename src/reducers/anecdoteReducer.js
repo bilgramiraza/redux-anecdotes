78f8776aeef54662a71db0da267c34376a5c7602
Raza <bilgramiraza@yahoo.com>
2024-09-09 15:48:28 +0530

    Restructured Initialization of the app using Redux Thunks

--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit"
+import anecdoteService from '../services/anecdotes';
 
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
@@ -23,5 +24,12 @@ export const {
   add,
   setAnecdotes,
 } = anecdoteSlice.actions;
+
+export const initializeAnecdotes = () => {
+  return async dispatch => {
+    const anecdotes = await anecdoteService.getAll();
+    dispatch(setAnecdotes(anecdotes));
+  };
+};
 
 export default anecdoteSlice.reducer;
