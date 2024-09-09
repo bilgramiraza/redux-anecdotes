beb88e336f53db77c7faf25abb796328980da2aa
Raza <bilgramiraza@yahoo.com>
2024-09-09 15:17:39 +0530

    Implemented Posting New Anecdotes to the backend

--- src/components/AnecdoteForm.jsx
+++ src/components/AnecdoteForm.jsx
@@ -1,17 +1,21 @@
 import { useDispatch } from "react-redux";
 import { add } from "../reducers/anecdoteReducer";
 import { setNotify } from "../reducers/notificationReducer";
+import anecdoteService from '../services/anecdotes';
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
 
-  const addAnecdote = event => {
+  const addAnecdote = async event => {
     event.preventDefault();
-    const anecdote = event.target.anecdote.value;
-    dispatch(add(anecdote));
+    const content = event.target.anecdote.value;
+
+    const newAnecdote = await anecdoteService.create(content);
+    dispatch(add(newAnecdote));
+
     event.target.anecdote.value = '';
     dispatch(setNotify({
-      message: `New Anecdote: ${anecdote}`,
+      message: `New Anecdote: ${newAnecdote.content}`,
       timer: 5000,
     }));
   };
